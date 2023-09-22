bc639d526e1049ae6dd2d2c06ecbb8b25ba375f9
Victor <aue228lublupacanov@gmail.com>
2023-09-22 15:19:40 +0300

    second input added

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,20 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [desc, setDesc] = useState("");
+  const [desc, setDesc] = useState({ desctiption: "", date: "" });
   const [todos, setTodos] = useState([]);
 
-  const inputChange = (event) => {
-    setDesc(event.target.value);
+  const inputDescription = (event) => {
+    setDesc({ ...desc, desctiption: event.target.value });
+  }
+  const inputDate = (event) => {
+    setDesc({ ...desc, date: event.target.value });
   }
 
   const addTodo = (event) => {
     event.preventDefault();
     setTodos([...todos, desc]);
+    console.log(todos);
   }
   return (
 
@@ -19,16 +23,24 @@ function App() {
       <h1>TodoList</h1>
       <form onSubmit={addTodo}>
         <label>Description:</label>
-        <input type='text' value={desc} onChange={inputChange} />
+        <input type='text' value={desc.desctiption} onChange={inputDescription} />
         <label>Date:</label>
-        <input type='text' value={desc} onChange={inputChange} />
+        <input type='text' value={desc.date} onChange={inputDate} />
         <input type='submit' value="add" />
       </form>
       <table>
         <tbody>
+          <tr>
+            <th>Description</th>
+            <th>Date</th>
+          </tr>
+          <hr />
           {todos.map((todo, index) =>
             <tr key={index}>
-              <td>{todo}</td>
+              <td>{todo.desctiption}</td>
+
+              <td>{todo.date}</td>
+
             </tr>
           )}
         </tbody>
