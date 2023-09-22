400e1c346aa388ef584bc7e98e13c91b397cc628
Victor <aue228lublupacanov@gmail.com>
2023-09-22 13:15:07 +0300

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,38 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [desc, setDesc] = useState("");
+  const [todos, setTodos] = useState([]);
 
+  const inputChange = (event) => {
+    setDesc(event.target.value);
+  }
+
+  const addTodo = (event) => {
+    event.preventDefault();
+    setTodos([...todos, desc]);
+  }
   return (
+
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>TodoList</h1>
+      <form onSubmit={addTodo}>
+        <label>Description:</label>
+        <input type='text' value={desc} onChange={inputChange} />
+        <label>Date:</label>
+        <input type='text' value={desc} onChange={inputChange} />
+        <input type='submit' value="add" />
+      </form>
+      <table>
+        <tbody>
+          {todos.map((todo, index) =>
+            <tr key={index}>
+              <td>{todo}</td>
+            </tr>
+          )}
+        </tbody>
+      </table>
     </>
   )
 }
